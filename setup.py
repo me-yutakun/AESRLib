7a36c4a6b7537d3073cef7051ae75d0148da54bf
Yuta-kun <cosmoscandium@gmail.com>
2022-05-03 17:16:04 +0530

    Cleared and added setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,23 @@ setup(
   name = 'AESRLib',
   packages = ['AESRLib'],
   version = 'v1.0-beta',
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Crypto package based on var key with mix of AES and Randomization',   # Give a short description about your library
-  author = 'Shubham Chakraborty',                   # Type in your name
-  author_email = 'cosmoscandium@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  license='MIT',
+  description = 'Crypto package based on var key with mix of AES and Randomization',
+  author = 'Shubham Chakraborty',
+  author_email = 'cosmoscandium@gmail.com',
+  url = 'https://github.com/me-yutakun/AESRLib',
+  download_url = 'https://github.com/me-yutakun/AESRLib/archive/refs/tags/v1.0-beta.tar.gz',
+  keywords = ['AES', 'Random', 'base64'],
+  install_requires=[
+          'pycryptodome'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.10',
   ],
 )
